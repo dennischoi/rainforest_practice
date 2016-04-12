37593a2138d65fd741eaf2f7a8a2e70e5dc909c7
Dennis Choi <dennis.choi7@gmail.com>
2016-04-12 15:17:25 -0400

    Items controller and views

---
+++ rainforest2/app/controllers/items_controller.rb
@@ -0,0 +1,50 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(item_params)
+      redirect_to item_path(@item)
+    else
+      render :exit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price_in_cents)
+  end
+
+
+
+end
