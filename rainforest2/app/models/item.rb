417201648761577ff289555f43ef3faa9fa8d7fe
Dennis Choi <dennis.choi7@gmail.com>
2016-04-12 15:57:12 -0400

    Displaying in dollars... utilizing sprintf and to_floats

--- rainforest2/app/models/item.rb
+++ rainforest2/app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   validates :decription, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+
+  def formatted_price
+    binding.pry
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
+
+
 end
